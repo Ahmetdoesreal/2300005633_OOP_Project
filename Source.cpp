b945c357a65a48a0fb44afb01a4d2334d692e82a
Ahmetdoesreal <a.deniz.sezgin@gmail.com>
2024-11-17 02:50:46 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,2 +1,42 @@
 #include <iostream>
+#include <string>
 using namespace std;
+class OnlineCourse{
+private:
+    /* data */
+public:
+
+    OnlineCourse(/* args */);
+    ~OnlineCourse();
+};
+
+class SoftwareCourses:public OnlineCourse{
+private:
+    /* data */
+public:
+    SoftwareCourses(/* args */);
+    ~SoftwareCourses();
+};
+class LanguageCourses:public OnlineCourse{
+private:
+    /* data */
+public:
+    LanguageCourses(/* args */);
+    ~LanguageCourses();
+};
+class ProfessionalDevelopmentCourses:public OnlineCourse
+{
+private:
+    /* data */
+public:
+    ProfessionalDevelopmentCourses(/* args */);
+    ~ProfessionalDevelopmentCourses();
+};
+class Electives:public OnlineCourse
+{
+private:
+    /* data */
+public:
+    Electives(/* args */);
+    ~Electives();
+};
